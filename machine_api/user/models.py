6ec6e128df8f0b8547bec57df7a10d7db6e8bce0
unknown <aguptaworkspace@gmail.com>
2023-09-29 13:22:20 +0530

    added users with id

--- machine_api/user/models.py
+++ machine_api/user/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Client(models.Model):
     client_name = models.CharField(max_length=50)
     uid = models.ForeignKey(User,on_delete=models.CASCADE,db_column='uid')
+    created_at = models.DateTimeField(default=datetime.datetime.now())
 
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
--- machine_api/user/views.py
+++ machine_api/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt #used to bypass csrf token
+from .models import Client, Project,User
 import json
 
 # Create your views here.
@@ -11,6 +12,21 @@ def get_client(request):
 
 @csrf_exempt                        #decorator used to bypass csrf token
 def create_client(request):
+
+
+    print(request.method)
+    print(request.POST)
+    print(type(request.POST))
+    
+    
+    cname = request.POST['cname']
+    user_id =  request.POST['user_id']
+    u = User.objects.filter(id=user_id)  #select * from auth_user where id=user_id;
+    print(u)
+    print(u[0])
+    c = Client.objects.create(client_name=cname,uid=u[0])
+    c.save()
+
     res = {'success':'response from post client'}
     json_data = json.dumps(res)
     return HttpResponse(json_data)
