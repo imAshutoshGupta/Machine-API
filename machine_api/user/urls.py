595200cb3f54dbe2974a0e3996237071099e3fc6
unknown <aguptaworkspace@gmail.com>
2023-09-29 12:31:59 +0530

    made all views for crud

--- machine_api/user/urls.py
+++ machine_api/user/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user', views.user),
+    path('getclient', views.get_client),
+    path('createclient', views.create_client),
+    path('updateclient', views.update_client),
+    path('deleteclient', views.delete_client),
 ]
--- machine_api/user/views.py
+++ machine_api/user/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt #used to bypass csrf token
+import json
 
 # Create your views here.
-def user(request):
-    return HttpResponse("USER PAGE")
+def get_client(request):
+    res = {'success':'response from get client'}
+    json_data = json.dumps(res)
+    return HttpResponse(json_data)
+
+@csrf_exempt                        #decorator used to bypass csrf token
+def create_client(request):
+    res = {'success':'response from post client'}
+    json_data = json.dumps(res)
+    return HttpResponse(json_data)
+
+@csrf_exempt 
+def update_client(request):
+    res = {'success':'client updated successfully'}
+    json_data = json.dumps(res)
+    return HttpResponse(json_data)
+
+@csrf_exempt 
+def delete_client(request):
+    res = {'success':'deleted successfully'}
+    json_data = json.dumps(res)
+    return HttpResponse(json_data)
